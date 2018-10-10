7d814f1bc6a47918aea3c56c8662234ad0ebaa28
Ankita Khurana <ankita0096@gmail.com>
2018-10-10 15:58:12 +0530

    Error Handling for Service

--- src/app/login-box/login-box.component.ts
+++ src/app/login-box/login-box.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../User';
-import {LoginService} from '../login.service';
+import { LoginService } from '../login.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 
@@ -12,19 +12,22 @@ import { UseExistingWebDriver } from 'protractor/built/driverProviders';
 })
 export class LoginBoxComponent implements OnInit {
 
-  constructor(private loginService:LoginService, private formBuilder: FormBuilder) { }
-  
+  constructor(private loginService: LoginService, private formBuilder: FormBuilder) { }
+
   user: User;
   form: FormGroup;
 
   login(): void {
-    this.loginService.login(this.form.value).subscribe(user=>{
-      console.log(user);
-    }
+    this.loginService.login(this.form.value).subscribe(user =>
+      console.log(user),
+      error => {
+        console.log(error)
+      }
+    )
   }
   ngOnInit() {
     this.form = this.formBuilder.group({
-      username: [null,Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])],
+      username: [null, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(20)])],
       password: [null, Validators.required]
     });
   }
